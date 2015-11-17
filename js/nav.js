9f2be541c4eadd0067033df320aed9adec9c4270
Gautham Yerroju <gautham.yerroju@gmail.com>
2015-11-18 04:26:22 +0530

    Removed unused nav.js file.

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".menu-switch").click(function() {
-        $(".menu-switch").toggleClass("menu--open");
-        $(".slider").toggleClass("menu--open");
-    });
-});
